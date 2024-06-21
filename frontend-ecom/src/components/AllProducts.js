98fd0d185608847e2dcdac8c6918fca8b59febca
Shital Kandel <shital@Kali.Kali>
2024-06-21 20:01:47 +0545

    solved: product category fetch from backend

--- frontend-ecom/src/components/AllProducts.js
+++ frontend-ecom/src/components/AllProducts.js
@@ -3,36 +3,34 @@ import { Link } from 'react-router-dom';
 import SingleProduct from './SingleProduct';
 import { useState, useEffect } from 'react';
 
-function AllProduct() {
-    const baseUrl = 'http://localhost:8000/api'
+function AllProducts(props) {
+    const baseUrl = 'http://127.0.0.1:8000/api'
     const [products, setProducts] = useState([]);
     const [totalResult, setTotalResults] = useState(0);
-
 
     useEffect(() => {
         fetchData(baseUrl+'/products');
     }, []);
 
-
     function fetchData(baseurl) {
-        fetch(baseurl,{mode:'cors'})
+        fetch(baseurl)
             .then((response) => response.json())
             .then((data) => {
-                console.log(data);
+                console.log();
                 setProducts(data.results);
                 setTotalResults(data.count);
             });
     }
 
-
-    function changeUrl(url) {
-        fetchData(url);
+    const links = [];
+    for (let i = 1; i <= totalResult; i++){
+        links.push(<li className='page-item'>
+            <Link onClick={()=> changeUrl(baseUrl + `/products/?page=${i}`)} to={`/products/?page=${i}`} className="page-link">{i}</Link>
+            </li>);
     }
 
-    var links = [];
-    for (let i = 1; i <= totalResult; i++) {
-        links.push(<li class="page-item"><Link onClick={() => changeUrl( baseUrl + `/products/?page=${i}`)} to={'/products/?page=${i}'} class="page-link">{i}</Link></li>
-        )
+    function changeUrl(url){
+        fetchData(url)
     }
 
     return (
@@ -47,13 +45,12 @@ function AllProduct() {
             {/* End Latest Product */}
 
             <nav aria-label="Page navigation example">
-
-                <ul class="pagination">
+                <ul className="pagination">
                     {links}
                 </ul>
             </nav>
         </section>
-    )
+    );
 }
 
-export default AllProduct;
+export default AllProducts;
